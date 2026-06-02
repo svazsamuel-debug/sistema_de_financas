46965d36aaeab0b2fe33645803506521d985b3e2
Samuel Vaz <svaz.samuel@gmail.com>
2026-06-02 20:37:12 -0300

    Adicionado histórico de movimentações no Extrato
    Adicionada a funcionalidade de Extrato com o histórico de transações

--- main.py
+++ main.py
@@ -8,16 +8,18 @@ def titulo(msg):
 # Programa Principal
 titulo('SISTEMA FINANCEIRO')
 saldo = 0
-
+movimentos = []
 while True:
     print('1 - Receita')
     print('2 - Despesa')
     print('3 - Saldo')
+    print('4 - Extrato')
     print('0 - Sair')
     escolha = int(input('\nEscolha uma das opções: '))
     sleep(1)
     if escolha == 1:
         rec = float(input('Digite o valor da Receita R$'))
+        movimentos.append({"tipo": "Receita", "valor": rec})
         print('Analisando...')
         sleep(1)
         print(f'O saldo anterior era de R${saldo:.2f} e foi adicionado R${rec:.2f}.')
@@ -27,6 +29,7 @@ while True:
         print('-'*60)
     elif escolha == 2:
         des = float(input('Digite o valor da Despesa R$'))
+        movimentos.append({"tipo" : "Despesa", "valor": des})
         print('Analisando...')
         sleep(1)
         print(f'O saldo atual era de R${saldo:.2f} e foi debitado o valor de R${des:.2f}.')
@@ -40,6 +43,18 @@ while True:
         sleep(1)
         print(f'O saldo atual é de R${saldo:.2f}.')
         print('-'*60)
+    elif escolha == 4:
+        sleep(1)
+        print('Buscando todas movimentações da conta...')
+        sleep(1)
+        titulo('EXTRATO ATUALIZADO')
+        if len(movimentos) == 0:
+            print('Sem movimentações na conta!')
+        else:
+            for c in movimentos:
+                print(f'{c["tipo"]:<10} R${c["valor"]:10}')
+            print('-'*60)
+            print(f'Saldo atual R${saldo:.2f}')
     elif escolha == 0:
         print('Saindo do sistema...')
         sleep(1)
