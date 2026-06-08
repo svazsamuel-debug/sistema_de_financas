a8a04facacf87a44787f98db398a79d8a31c37e0
Samuel Vaz <svaz.samuel@gmail.com>
2026-06-08 18:21:10 -0300

    Adiciona ajustes na formatação das informações

--- main.py
+++ main.py
@@ -19,39 +19,50 @@ while True:
     sleep(1)
     if escolha == 1:
         rec = float(input('Digite o valor da Receita R$'))
-        desc = str(input('Descrição (salário, presente, freelance: '))
+        desc = str(input('Descrição (salário, presente, freelance): '))
         movimentos.append({"tipo": "Receita", "valor": rec, "desc": desc})
+        print('-'*60)
         print('Analisando...')
+        print('-'*60)
         sleep(1)
-        print(f'O saldo anterior era de R${saldo:.2f} e foi adicionado R${rec:.2f}.')
+        print('-'*60)
+        print(f'SALDO ANTERIOR R${saldo:.2f}')
+        print('-'*60)
         sleep(1)
         saldo += rec
-        print(f'Saldo Atual R${saldo:.2f}')
+        print(f'Saldo ATUAL R${saldo:.2f}')
         print('-'*60)
     elif escolha == 2:
         des = float(input('Digite o valor da Despesa R$'))
         if des > saldo:
-            print(f'Saldo Insuficiente.'
+            print(f'Não é possível realizar essa movimentação. Revise seu saldo.'
                   f'Saldo Atual R${saldo:.2f}')
         else:
             desc = str(input('Descrição (alimentação, lazer, aluguel: '))
             movimentos.append({"tipo" : "Despesa", "valor": des, "desc": desc})
+            print('-'*60)
             print('Analisando...')
+            print('-'*60)
             sleep(1)
-            print(f'O saldo atual era de R${saldo:.2f} e foi debitado o valor de R${des:.2f}.')
+            print(f'SALDO ANTERIOR R${saldo:.2f} e foi debitado o valor de R${des:.2f}.')
+            print('-'*60)
             sleep(1)
             saldo -= des
             print(f'Saldo atual R${saldo:.2f}')
         print('-'*60)
     elif escolha == 3:
         sleep(1)
+        print('-'*60)
         print('Calculando o saldo atual...')
+        print('-'*60)
         sleep(1)
         print(f'O saldo atual é de R${saldo:.2f}.')
         print('-'*60)
     elif escolha == 4:
         sleep(1)
+        print('-'*60)
         print('Buscando todas movimentações da conta...')
+        print('-'*60)
         sleep(1)
         titulo('EXTRATO ATUALIZADO')
         if len(movimentos) == 0:
