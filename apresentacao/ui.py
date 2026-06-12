6c41df0b1a81b0ddc92c4468cac4500eea62ad75
Samuel Vaz <svaz.samuel@gmail.com>
2026-06-11 22:20:47 -0300

    Adiciona descrição nas funções
    Descrições adicionadas nas funções

--- apresentacao/ui.py
+++ apresentacao/ui.py
@@ -1,12 +1,32 @@
 def titulo(msg):
     '''
-    
+    titulo(msg)
+    Função para padronizar os títulos utilizados no decorrer do sistema
+    param msg: frase a ser destacada
+    retorna a mensagem formatada
+    ==============================
+          MENSAGEM DE EXEMPLO
+    ==============================
+    Função criada por Samuel Vaz com base em aulas do Prof Guanabara
     '''
     texto('='*60, 3)
     texto(msg.center(60), 3)
     texto('='*60, 3)
 
 def texto(msg, cores = 0):
+    '''
+    texto(msg, cores = 0)
+    Função para atribuir cores às respostas de modo mais direto
+    param msg: frase a ser colorida
+    param cores: cor a ser escolhida pelo usuário
+    0 - Sem cor (Cor padrão do terminal)
+    1 - Vermelho
+    2 - Verde
+    3 - Branco
+    4 - Azul
+    5 - Roxo
+    Função criada por Samuel Vaz
+    '''
     print(f'{cor[cores]} {msg.ljust(60)} {cor[0]}')
 
 cor = ('\033[m',          # 0 - Sem cor
